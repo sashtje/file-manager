d34d6eda2a3f6d53173ae9998c396fd073e7552c
sashtje <sashtje.lefevre@gmail.com>
2023-12-18 22:33:18 +0300

    feat: add showWelcome

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+function getUsername() {
+  let usernameArg = process.argv[2];
+  let usernameArgName = "username";
+  let username = "Anonymous";
+
+  if (usernameArg) {
+    usernameArg = usernameArg.slice(2).split("=");
+
+    if (usernameArg[0] === usernameArgName && usernameArg[1]) {
+      username = usernameArg[1];
+    }
+  }
+
+  return username;
+}
+
+function showWelcome(username) {
+  console.log(`Welcome to the File Manager, ${username}!`);
+}
+
+function start() {
+  let username = getUsername();
+  showWelcome(username);
+}
+
+start();
