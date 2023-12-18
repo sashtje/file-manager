96406a884dd041fca99b64798c92599a7ae2d0f2
sashtje <sashtje.lefevre@gmail.com>
2023-12-19 00:37:12 +0300

    feat: add showGoodbye and showPrompt

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { createInterface } from "node:readline";
+
 function getUsername() {
   let usernameArg = process.argv[2];
   let usernameArgName = "username";
@@ -14,13 +16,48 @@ function getUsername() {
   return username;
 }
 
+function showPrompt() {
+  process.stdout.write("> ");
+}
+
 function showWelcome(username) {
   console.log(`Welcome to the File Manager, ${username}!`);
 }
 
+function showGoodbye(username) {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+}
+
 function start() {
   let username = getUsername();
+
   showWelcome(username);
+  showPrompt();
+
+  let rl = createInterface({
+    input: process.stdin,
+    output: process.strdout,
+  });
+
+  rl.on("line", (input) => {
+    let line = input.trim();
+
+    if (line === ".exit") {
+      process.emit("SIGINT");
+      return;
+    }
+
+    showPrompt();
+  });
+
+  if (process.platform === "win32") {
+    rl.on("SIGINT", () => process.emit("SIGINT"));
+  }
+
+  process.on("SIGINT", () => {
+    showGoodbye(username);
+    process.exit();
+  });
 }
 
 start();
