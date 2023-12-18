35b543c316b043b3eb70e36fa36d79186d257f94
sashtje <sashtje.lefevre@gmail.com>
2023-12-19 01:14:59 +0300

    feat: add showCurrentDir

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createInterface } from "node:readline";
+import { homedir } from "node:os";
 
 function getUsername() {
   let usernameArg = process.argv[2];
@@ -24,15 +25,26 @@ function showWelcome(username) {
   console.log(`Welcome to the File Manager, ${username}!`);
 }
 
-function showGoodbye(username) {
+function showGoodbye({ username, lineBreaks }) {
+  if (lineBreaks) {
+    process.stdout.write("\n".repeat(lineBreaks));
+  }
+
   console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+}
+
+function showCurrentDir(dir) {
+  console.log(`\nYou are currently in ${dir}`);
+}
+
+function showInvitation(dir) {
+  showCurrentDir(dir);
+  showPrompt();
 }
 
 function start() {
   let username = getUsername();
-
-  showWelcome(username);
-  showPrompt();
+  let currentDir = homedir();
 
   let rl = createInterface({
     input: process.stdin,
@@ -43,11 +55,13 @@ function start() {
     let line = input.trim();
 
     if (line === ".exit") {
-      process.emit("SIGINT");
-      return;
+      showGoodbye({ username, lineBreaks: 1 });
+      process.exit();
     }
 
-    showPrompt();
+    // manager
+
+    showInvitation(currentDir);
   });
 
   if (process.platform === "win32") {
@@ -55,9 +69,12 @@ function start() {
   }
 
   process.on("SIGINT", () => {
-    showGoodbye(username);
+    showGoodbye({ username, lineBreaks: 2 });
     process.exit();
   });
+
+  showWelcome(username);
+  showInvitation(currentDir);
 }
 
 start();
