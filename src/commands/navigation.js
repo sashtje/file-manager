41f02320bf4d9503cbd406be8fa2a05546c59114
sashtje <sashtje.lefevre@gmail.com>
2023-12-22 00:23:56 +0300

    feat: add ls code

--- src/commands/navigation.js
+++ src/commands/navigation.js
@@ -38,10 +38,39 @@ export class Navigation {
     try {
       await readdir(pathToDirectory);
       UserInfo.currentDirectory = pathToDirectory;
-    } catch (err) {
+    } catch {
       throw new OperationFailedError("no such directory");
     }
   }
 
-  static ls() {}
+  static async ls() {
+    const DIRECTORY = "directory";
+    const FILE = "file";
+
+    try {
+      const filesAndFolders = await readdir(UserInfo.currentDirectory, {
+        withFileTypes: true,
+      });
+      const filesAndFoldersSorted = filesAndFolders
+        .map((item) => ({
+          Name: item.name,
+          Type: item.isFile() ? FILE : DIRECTORY,
+        }))
+        .sort((a, b) => {
+          if (a.Type === DIRECTORY && b.Type === FILE) {
+            return -1;
+          }
+
+          if (a.Type === FILE && b.Type === DIRECTORY) {
+            return 1;
+          }
+
+          return a.Name.localeCompare(b.Name);
+        });
+
+      console.table(filesAndFoldersSorted);
+    } catch (err) {
+      throw new OperationFailedError(err.message);
+    }
+  }
 }
