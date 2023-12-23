bfcca94377f4289a0dfa96814a53cfd3a3afef5e
sashtje <sashtje.lefevre@gmail.com>
2023-12-23 23:44:49 +0300

    feat: add os code

--- src/commands/os-info.js
+++ src/commands/os-info.js
@@ -1,5 +1,48 @@
-export class Os {
-  // ["--EOL", "--cpus", "--homedir", "--username", "--architecture"]
+import { EOL, cpus, homedir, userInfo, arch } from "node:os";
 
-  static os(flag) {}
+import { InvalidInputError, OperationFailedError } from "../helpers/errors.js";
+
+export class Os {
+  static os(flag) {
+    if (!flag) {
+      throw new InvalidInputError("no flag specified");
+    }
+
+    try {
+      switch (flag) {
+        case "--EOL":
+          if (EOL === "\n") {
+            console.log("\\n");
+          } else {
+            console.log("\\r\\n");
+          }
+          return;
+
+        case "--cpus":
+          const data = cpus();
+          console.log("Total CPUs count: ", data.length);
+          data.forEach((cpu, index) =>
+            console.log(`CPU №${index + 1}: model: ${cpu.model}`)
+          );
+          return;
+
+        case "--homedir":
+          console.log(homedir());
+          return;
+
+        case "--username":
+          console.log(userInfo().username);
+          return;
+
+        case "--architecture":
+          console.log(arch());
+          return;
+
+        default:
+          throw new InvalidInputError("flag not supported");
+      }
+    } catch (err) {
+      throw new OperationFailedError(err.message);
+    }
+  }
 }
