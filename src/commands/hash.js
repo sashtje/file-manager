75c53ca6f2794903801fbff8b8225733ec99f8b3
sashtje <sashtje.lefevre@gmail.com>
2023-12-24 00:18:22 +0300

    feat: add hash code

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,3 +1,32 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+
+import { getCommandArgsWithAbsolutePath } from "../helpers/getArgsFromArgsChunks.js";
+import { InvalidInputError, OperationFailedError } from "../helpers/errors.js";
+
 export class Hash {
-  static hash(pathToFile) {}
+  static async hash(...chunksArgs) {
+    let [pathToFile, ...rest] = getCommandArgsWithAbsolutePath(chunksArgs);
+
+    if (!pathToFile) {
+      throw new InvalidInputError("file path not specified");
+    }
+
+    if (rest.length) {
+      throw new InvalidInputError("too many arguments");
+    }
+
+    try {
+      const hash = createHash("sha256");
+      const input = createReadStream(pathToFile);
+
+      await pipeline(input, hash.setEncoding("hex"), process.stdout, {
+        end: false,
+      });
+      console.log("");
+    } catch (err) {
+      throw new OperationFailedError(err.message);
+    }
+  }
 }
