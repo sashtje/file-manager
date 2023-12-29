6c06e65cb39280e651b4803e54ceb9543f9e3081
sashtje <sashtje.lefevre@gmail.com>
2023-12-29 13:41:23 +0300

    feat: add sign for errors

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -1,13 +1,13 @@
 export class InvalidInputError extends Error {
   constructor(message) {
-    super(`Invalid input: ${message}`);
+    super(`⛔ Invalid input: ${message}`);
     this.name = this.constructor.name;
   }
 }
 
 export class OperationFailedError extends Error {
   constructor(message) {
-    super(`Operation failed: ${message}`);
+    super(`⛔ Operation failed: ${message}`);
     this.name = this.constructor.name;
   }
 }
